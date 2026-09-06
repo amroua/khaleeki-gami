43aabaa46f2d5d8c553d343ef7f745b495fff7c5
amroua <dedlek456@gmail.com>
2026-09-07 02:45:44 +0300

    Refactor handler to export app directly
    Simplified the handler by directly exporting the app without service initialization.h

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,3 @@
-import app, { initServices } from '../server/app';
+import app from '../server/app.ts';
 
-let isReady = false;
-
-export default async function handler(req: any, res: any) {
-  if (!isReady) {
-    try {
-      await initServices();
-      isReady = true;
-    } catch (err) {
-      console.error('[Vercel Serverless] Service init warning:', err);
-    }
-  }
-
-  return app(req, res);
-}
+export default app;
